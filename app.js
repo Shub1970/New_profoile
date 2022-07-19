4e440230b8e4aade06a4ba316ffd45f98ed9bde2
Shub1970 <18shubham1998@gmail.com>
2022-07-20 01:00:24 +0530

    update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 30;
+    var elementVisible = 35;
     console.log(elementTop, windowHeight);
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
